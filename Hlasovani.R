b30fa2888eb80ad9dfaf57c9648a768de2a2d8bb
jfronc <122388202+jfronc@users.noreply.github.com>
2023-04-27 17:14:46 +0200

    Update Hlasovani.R

--- Hlasovani.R
+++ Hlasovani.R
@@ -1,1 +1,2 @@
-unl_data <- read.table("C:/Users/jarom/Downloads/hl-1993ps/hl1993s.unl", sep="|", header=FALSE)
+hl1993 <- read.table("C:/Users/jarom/Downloads/hl-1993ps/hl1993s.unl", sep="|", header=FALSE, fileEncoding = "windows-1250")
+colnames(hl1993) <- c("id_hlasovani", "id_organ", "schuze", "cislo", "bod", "datum", "cas", "pro", "proti", "zdrzel", "nehlasoval", "prihlaseno", "kvorum", "druh_hlasovani", "vysledek", "nazev_dlouhy", "nazev_kratky")
