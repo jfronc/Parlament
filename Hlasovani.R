e4383f31d35e794cf26b20b0d618b18fa5b43333
jfronc <122388202+jfronc@users.noreply.github.com>
2023-04-29 21:53:52 +0200

    Update Hlasovani.R

--- Hlasovani.R
+++ Hlasovani.R
@@ -14,7 +14,13 @@ colnames(osoby) <- c('id', 'title_pre', 'family_name', 'given_name', 'title_post
 organy <- read.table("C:/Users/jarom/Downloads/poslanci/organy.unl", sep="|", header=FALSE, fileEncoding = "windows-1250")
 colnames(organy) <- c('org_id', 'sup_org_id', 'type_org_id', 'org_abbreviation', 'org_name_cs', 'org_name_en', 'org_since', 'org_until', 'priority', 'members_base', 'dummy')
 
+kluby <- filter(organy, type_org_id == 1)
+
 zarazeni <- read.table("C:/Users/jarom/Downloads/poslanci/zarazeni.unl", sep="|", header=FALSE, fileEncoding = "windows-1250")
 colnames(zarazeni) <- c('id', 'of_id', 'cl_funkce', 'since', 'until', 'since_f', 'until_f', 'dummy')
 zarazeni$since %<>% as.Date()
 zarazeni$until %<>% as.Date()
+
+zarazeni_kluby <- zarazeni %>% filter(of_id %in% kluby$org_id)
+
+zarazeni_kluby %>% filter(since <= as.Date("2023-01-01") & (until >= as.Date("2022-01-01") | is.na(until))) %>% group_by(of_id) %>% summarize(count = n())
