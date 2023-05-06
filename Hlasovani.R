ff6a7295d75f66de1504203665e5af0cbf9e4341
jfronc <122388202+jfronc@users.noreply.github.com>
2023-05-06 19:50:19 +0200

    Update Hlasovani.R

--- Hlasovani.R
+++ Hlasovani.R
@@ -30,21 +30,26 @@ zarazeni$until %<>% as.Date()
 
 kluby_n <- function(date) {
   zarazeni %>% 
-  filter(cl_funkce == 0) %>%  # nutné odfiltroval řádky s fcí v PK; pokud cl_funkce == 1, pak id_of odpovídá funkce:id_funkce.
+  filter(cl_funkce == 0) %>%  # nutné odfiltrovat řádky s fcí v PK; pokud cl_funkce == 1, pak id_of odpovídá funkce:id_funkce.
   filter(org_id %in% kluby$org_id) %>%  left_join(kluby %>% select(org_id, org_abbreviation), by = "org_id") %>%
-  filter(since <= as.Date(date, origin = "1970-01-01") & (until >= as.Date(date, origin = "1970-01-01") | is.na(until))) %>% group_by(org_abbreviation) %>% summarize(count = n()) %>% bind_rows(data.frame(org_abbreviation = "Celkem", count = sum(.$count)))
+  filter(since <= as.Date(date, origin = "1970-01-01") & (until > as.Date(date, origin = "1970-01-01") | is.na(until))) %>% group_by(org_abbreviation) %>% summarize(count = n()) %>% bind_rows(data.frame(org_abbreviation = "Celkem", count = sum(.$count)))
 }
 
 # Vývoj počtu mandátů
-dates <- seq(as.Date("2018-01-01"), as.Date("2022-01-01"), by = "month")
+dates <- unique(zarazeni$since)
+dates <- dates[dates >= as.Date("1992-01-01", origin = "1970-01-01")]
+
 kluby_df <- data.frame() # initialize empty data frame
-  for (date in dates) {
-    kluby_count <- kluby_n(date)
-    kluby_count$date <- as.Date(date, origin = "1970-01-01")
-    kluby_df <- rbind(kluby_df, kluby_count)
+
+for (date in dates) {
+  kluby_count <- kluby_n(date)
+  kluby_count$date <- as.Date(date, origin = "1970-01-01")
+  kluby_df <- rbind(kluby_df, kluby_count)
  }
+
 library(tidyr)
 kluby_df_pivoted <- pivot_wider(kluby_df, names_from = org_abbreviation, values_from = count, values_fill = 0) %>% 
+  filter(Celkem >= 197 & Celkem <= 200) %>%  # odstraní řádky okolo přelomu VO
   distinct(across(-date), .keep_all = TRUE) # odstraní řádky, kde nedošlo ke změně počtů
 
 # Kdo byl v klubu ODS ke dni?
