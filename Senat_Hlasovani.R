b5cd6c75160af2d3b17b4b0c1a841e56b95b7f2d
jfronc <122388202+jfronc@users.noreply.github.com>
2024-12-13 15:46:16 +0100

    zobecnění pro různá funkční období, úprava čísel v grafu

--- Senat_Hlasovani.R
+++ Senat_Hlasovani.R
@@ -4,9 +4,10 @@ xfun::pkg_attach("tidyverse", "magrittr", "xml2")
 
 division = function (nr) {
   base_url = 'https://www.senat.cz/xqw/xervlet/pssenat/hlasy?G='
-  page = paste0(base_url, nr) %>% read_html()
+  page = paste0(base_url, nr) %>% read_html(encoding = "windows-1250")
   pro = list()
   proti = list()
+  clubs = xml_find_all(page, "//div[@class='wrapper-table']//h2") %>% xml_text()
   tables = xml_find_all(page, "//table[@class='PE_zebra outlineRows']")
   for (i in seq_along(tables))  {
     table = tables[i] %>% xml_find_all(".//tr") %>% xml_text()
@@ -15,17 +16,20 @@ division = function (nr) {
   }
   df = data.frame(unlist(pro), unlist(proti))
   colnames(df) = c("pro", "proti")
-  df$klub <- c("ODS+TOP", "STAN", "KDU", "ANO+SOCDEM", "SEN21+Pir", "Nez")
-  
+  df$klub <- clubs %>% str_replace("Senátorský klub ", "") %>% str_replace ("Občanské demokratické strany", "ODS")
+    
   df %<>% pivot_longer(cols = c(pro, proti), names_to = "Position", values_to = "Votes")
   
   ggplot(df, aes(x = Position, y = Votes, fill = klub)) +
     geom_bar(position="stack", stat = "identity", width = 0.5) +
     labs(title = "Pro a proti podle klubů", x = "Hlasování", y = "Hlasy") +
-    geom_text(aes(label = ifelse(Votes >= 2, Votes, "")), size = 5, position = position_stack(vjust = 0.5), check_overlap = TRUE) +
+    geom_text(aes(label = ifelse(Votes >= 3, Votes, "")), size = 5, position = position_stack(vjust = 0.5)) +
     theme_minimal()
 }
 
+# Čtrnácté funkční období 2022-2024
 # division(22488)
+# Patnácté funkční období 2024-2026
+# division(22693)
 
-# TODO: zobecnit pro různá funkční období, nastavit barvy
+# TODO: nastavit barvy
